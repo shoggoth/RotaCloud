4bfd89a4cb87c42fa4c62c112010d6196dc74a9d
rich@anubis <r.j.henry@gmail.com>
2020-02-17 12:42:09 +0000

    Initial Commit

---
+++ RotaCloud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RotaCloud
+//
+//  Created by Richard Henry on 17/02/2020.
+//  Copyright © 2020 Dogstar Industries Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
